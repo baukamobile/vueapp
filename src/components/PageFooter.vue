5aad9d4991cbd71ae045d53f8bb206562986a143
guidejet <guidejet@gmail.com>
2025-02-20 16:59:36 +0500

    footer removed

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -11,22 +11,9 @@
         <div
             class="flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400"
         >
-            <span>Hand-crafted & Made with</span>
-            <div class="flex items-center">
-                <span class="sr-only">love</span>
-                <span
-                    aria-hidden="true"
-                    class="iconify h-5 w-5 text-red-500 tabler--heart-filled"
-                ></span>
-            </div>
-            <span>by</span>
-            <a
-                href="https://github.com/Kamona-WD"
-                target="_blank"
-                class="text-blue-600 hover:underline"
-            >
-                Ahmed Kamel
-            </a>
+        
+           
+          
         </div>
     </footer>
 </template>
